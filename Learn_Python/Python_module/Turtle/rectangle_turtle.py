0a20a6e359c0a2faa16012ea0949de349caff775
daiwikgarg <daiwik.garg04@gmail.com>
2020-08-30 17:59:26 +0400

    test

---
+++ Learn_Python/Python_module/Turtle/rectangle_turtle.py
@@ -0,0 +1,13 @@
+import turtle
+import time
+s = turtle.Screen()
+s.bgcolor("light blue")
+t=turtle.Turtle()
+t.fd(200)
+t.rt(90)
+t.fd(100)
+t.rt(90)
+t.fd(200)
+t.rt(90)
+t.fd(100)
+time.sleep(20)
